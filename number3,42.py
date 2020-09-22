8030b3d520d1f7a65685ab5a3ca9083436761b45
Nazar <71321588+q005az@users.noreply.github.com>
2020-09-22 09:57:40 +0700

    Create number3,42.py

---
+++ number3,42.py
@@ -0,0 +1,12 @@
+print("Сколько лет Антону?")
+a= int(input())
+print("сколько лет Борису?")
+b= int(input())
+print("Сколько лет Виктору?")
+c= int(input())
+if a<=b>=c:
+    print(b)
+elif b<=a>=c:
+    print(a)
+else:
+    print(c)
